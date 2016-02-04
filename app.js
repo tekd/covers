ad7157191bb9ddf9605207ebf8bfc5ee9870c2b7
Denny Tek <tek.denny@gmail.com>
2016-02-03 21:01:56 -0500

    Set up express

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express();
+
+var port = 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function(req, res){
+  res.send("Hello Denny");
+});
+
+app.listen(port, function(err) {
+  console.log("running server on port " + port);
+});
